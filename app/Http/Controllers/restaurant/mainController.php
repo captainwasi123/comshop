07e7fb8b0e7900aed7132d87307ebd91dcdeda01
unknown <anasqamar.work@gmail.com>
2022-01-07 19:31:10 +0500

    Project Initialized

---
+++ app/Http/Controllers/restaurant/mainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\restaurant;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class mainController extends Controller
+{
+    //
+    function index(){
+
+    	return view('restaurant.dashboard');
+    }
+}
