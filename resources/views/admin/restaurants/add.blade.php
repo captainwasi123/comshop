8c59af9062adef3b827a0c0aec8f51d2f036dac1
anasQamarWork <anasqamar.work@gmail.com>
2022-02-18 22:04:43 +0500

    Anas 18-02-2022

--- resources/views/admin/restaurants/add.blade.php
+++ resources/views/admin/restaurants/add.blade.php
@@ -7,16 +7,24 @@
 <style type="text/css">
     .img-thumbnail {
     height: 150px;
-    width: 80%;
+    width: 75%;
 }
 .browseProfilePhoto {
     margin-top: 50px;
+}
+.previewProfilePhoto {
+    position: absolute;
+    left: 0px;
 }
 
 @media screen and (max-width:519px) and (min-width:320px) { 
 .browseProfilePhoto {
     margin-top: 10px;
     margin-left: 8px;
+}
+.previewProfilePhoto {
+    top: 18px;
+    left: 15px;
 }
 }
 </style>
@@ -48,14 +56,10 @@
                                 <div class="form-row">                                    
                                     <div class="col-lg-12 col-md-12 col-12 no-margin">
                                         <div class="row">
-<<<<<<< HEAD
                                             <div class="col-lg-2 col-md-4 col-8">
-                                                <img src="{{URL::to('/public/admin/assets')}}/images/placeholder.png" id="previewProfilePhoto" class="img-thumbnail">                                            
-=======
-                                            <div class="col-lg-2 col-md-5 col-5">
-                                                <img src="{{URL::to('/public/admin/assets')}}/images/placeholder.png" class="previewProfilePhoto previewProfilePhotoRes img-thumbnail">                                            
->>>>>>> 6360cb6f610be45e3ebbfe1c30b8dd4980c2a9b5
-                                            </div>
+                                                <img src="{{URL::to('/public/admin/assets')}}/images/placeholder.png" id="previewProfilePhoto" class="img-thumbnail"> 
+                                                <img src="{{URL::to('/public/admin/assets')}}/images/placeholder.png" class="previewProfilePhoto previewProfilePhotoRes img-thumbnail">
+                                            </div>                                           
                                             <div class="col-lg-10 col-md-8 col-12">
                                                 <div id="msg"></div>
                                                     <input type="file" name="logo_img" class="profilePic profilePicRes" accept="image/*">
@@ -80,16 +84,16 @@
                                             <input type="text" name="owner_name" class="form-control" required>
                                             <span class="text-danger" id="CurrentPasswordErrorMsg"></span>
                                         </div>
-                                    </div>    
-                                </div>
-                                <div class="form-row">                                  
+                                    </div>  
                                     <div class="col-lg-4 col-md-4 col-12 no-margin">
                                         <div class="input-form res-section-1">
                                             <label for="inputCurrentPassword"  class="no-margin pad-bot-10">Email</label>
                                             <input type="email" name="email" class="form-control" required>
                                             <span class="text-danger" id="CurrentPasswordErrorMsg"></span>
                                         </div>
-                                    </div>
+                                    </div>  
+                                </div>
+                                <div class="form-row">                                  
                                      <div class="col-lg-2 col-md-2 col-12 no-margin">
                                         <div class="input-form res-section-1">
                                             <label for="inputCurrentPassword"  class="no-margin pad-bot-10">Phone</label>
@@ -104,8 +108,6 @@
                                             <span class="text-danger" id="PasswordErrorMsg"></span>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="form-row">  
                                     <div class="col-lg-4 col-md-4 col-12 no-margin">
                                         <div class="input-form res-section-1">
                                             <label for="inputPassword" class="no-margin pad-bot-10">Password</label>
@@ -122,16 +124,16 @@
                                     </div>
                                 </div>
                                 <div class="form-row"> 
-                                    <div class="col-lg-8 col-md-8 no-margin">
+                                    <div class="col-lg-12 col-md-12 no-margin">
                                         <div class="input-form res-section-1">
                                             <label for="inputPassword" class="no-margin pad-bot-10">Address Details</label>
-                                            <textarea class="form-control" rows="3" name="address" id="add-input" required></textarea>
+                                            <textarea class="form-control" rows="5" name="address" id="add-input" required></textarea>
                                             <span class="text-danger" id="PasswordErrorMsg"></span>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="form-row"> 
-                                    <div class="col-lg-8 col-md-8 no-margin">
+                                    <div class="col-lg-12 col-md-12 no-margin">
                                         <div class="sav-button">
                                             <input type="Submit" value="Submit" class="bg-yellow">
                                         </div>
