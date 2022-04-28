25097aaaeef8366880a62bbf95b3cdd3b9227cd2
captainwasi123 <captain.wasi@gmail.com>
2022-04-29 03:05:06 +0500

    categories

--- routes/api.php
+++ routes/api.php
@@ -27,87 +27,72 @@ use App\Http\Controllers\Api\driver\driverprofileController;
 |
 */
 
-Route::get('getsingleRest/{id}', [bookingController::class, 'getsingleRest']);
 
-
-
-
-Route::post('UserRegister', [AuthController::class, 'UserRegister']);
-Route::post('DriverRegister', [driverprofileController::class, 'driverRegister']);
-
-Route::post('login', [AuthController::class, 'login']);
-
-
-Route::get('getCity', [getCityController::class, 'getCity']);
-Route::get('getCity/{id}', [getCityController::class, 'show']);
-
-
-
-
-
-
-
-Route::get('getCategories', [getCityController::class, 'getCategories']);
-Route::get('getmenu', [getCityController::class, 'getmenu']);
-
-
+//Buyer Authentication
+    Route::post('UserRegister', [AuthController::class, 'UserRegister']);
+    Route::post('login', [AuthController::class, 'login']);
 
 // DRIVER LOGIN
-Route::post('driverlogin', [driverAuthController::class, 'login']);
+    Route::post('driverlogin', [driverAuthController::class, 'login']);
+    Route::post('DriverRegister', [driverprofileController::class, 'driverRegister']);
 
 
+// General Routes
+    Route::get('getCity', [getCityController::class, 'getCity']);
+    Route::get('getCity/{id}', [getCityController::class, 'show']);
+    Route::get('getCategories', [getCityController::class, 'getCategories']);
+    Route::get('getmenu', [getCityController::class, 'getmenu']);
+
+    Route::get('getsingleRest/{id}', [bookingController::class, 'getsingleRest']);
 
 
 // User Middleware
-Route::middleware('auth:sanctum')->group( function () {
+    Route::middleware('auth:sanctum')->group( function () {
 
-    Route::post('profileUpdate', [profileController::class, 'profile_save']);
-    Route::post('changePassword', [profileController::class, 'changePassword']);
+        Route::post('profileUpdate', [profileController::class, 'profile_save']);
+        Route::post('changePassword', [profileController::class, 'changePassword']);
 
-    
-    Route::get('getRestuarant/{lat}/{lon}', [bookingController::class, 'getRestuarant']);
-    Route::get('searchRestMenu/{search}', [bookingController::class, 'searchRestMenu']);
+        
+        Route::get('getRestuarant/{lat}/{lon}', [bookingController::class, 'getRestuarant']);
+        Route::get('searchRestMenu/{search}', [bookingController::class, 'searchRestMenu']);
 
-//   menus with variant
-    Route::get('productDetail/{id}', [bookingController::class, 'productDetail']);
-
-
-
-// add to cart
-    Route::post('addCart', [cartController::class, 'addtocart']);
-    Route::get('ShowAllCart', [cartController::class, 'ShowAllCart']);
-    Route::post('updateCart/{id}', [cartController::class, 'updateCart']);
-    Route::post('destroyCart/{id}', [cartController::class, 'destroyCart']);
-
-// cart summary
-
-Route::get('cartSummary', [orderController::class, 'cartSummary']);
-    
-
-
-    // checkout
-    Route::post('checkout/{vat}/{pymenttype}/{lat}/{lon}', [orderController::class, 'store']);
-    
-Route::get('orderShow/{id}', [orderController::class, 'orderShow']);
-Route::post('orderUpdate/{id}', [orderController::class, 'orderUpdate']);
-Route::get('destroy/{id}', [orderController::class, 'destroy']);
-Route::get('search/{keyword}', [orderController::class, 'search']);
-   
-
-// get payment Method
-
-Route::get('paymentType', [getCityController::class, 'paymentType']);
+        //menus with variant
+            Route::get('productDetail/{id}', [bookingController::class, 'productDetail']);
 
 
 
-    Route::put('user/{id}',[AuthController::class,'update']);
-    Route::get('user/{id}',[AuthController::class,'show']);
+        // add to cart
+            Route::post('addCart', [cartController::class, 'addtocart']);
+            Route::get('ShowAllCart', [cartController::class, 'ShowAllCart']);
+            Route::post('updateCart/{id}', [cartController::class, 'updateCart']);
+            Route::post('destroyCart/{id}', [cartController::class, 'destroyCart']);
 
-    Route::post('logout',[AuthController::class,'logout']);
+        // cart summary
+            Route::get('cartSummary', [orderController::class, 'cartSummary']);
 
-  
+        // checkout
+            Route::post('checkout/{vat}/{pymenttype}/{lat}/{lon}', [orderController::class, 'store']);
+        
+        //Order
+            Route::get('orderShow/{id}', [orderController::class, 'orderShow']);
+            Route::post('orderUpdate/{id}', [orderController::class, 'orderUpdate']);
+            Route::get('destroy/{id}', [orderController::class, 'destroy']);
+            Route::get('search/{keyword}', [orderController::class, 'search']);
 
-});   
+        // get payment Method
+            Route::get('paymentType', [getCityController::class, 'paymentType']);
+
+
+
+        //User Gerneral
+            Route::put('user/{id}',[AuthController::class,'update']);
+            Route::get('user/{id}',[AuthController::class,'show']);
+
+            Route::post('logout',[AuthController::class,'logout']);
+
+      
+
+    });   
 
 
 
